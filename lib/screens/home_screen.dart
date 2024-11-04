aa0d421c3e69ef6cf6b774c5dc39cf11a1f38ebc
ssannccoss <sanjarbekalisherov837@gmail.com>
2024-11-04 18:10:47 +0500

    Updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -13,9 +15,8 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          systemOverlayStyle: const SystemUiOverlayStyle(
-            statusBarBrightness: Brightness.dark
-          ),
+          systemOverlayStyle:
+              const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark),
         ),
         body: Padding(
           padding: const EdgeInsets.fromLTRB(
@@ -24,12 +25,68 @@ class HomeScreen extends StatelessWidget {
             40,
             20,
           ),
+          // blurry background
           child: SizedBox(
             height: MediaQuery.of(context).size.width,
             child: Stack(
               children: [
                 Align(
-                  alignment: Alignment.,
+                  alignment: AlignmentDirectional(3, -0.3),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.deepPurple),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(-3, -0.3),
+                  child: Container(
+                    height: 300,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.deepPurple),
+                  ),
+                ),
+                Align(
+                  alignment: AlignmentDirectional(0, -2.5),
+                  child: Container(
+                    height: 300,
+                    width: 600,
+                    decoration: BoxDecoration(color: Colors.orange),
+                  ),
+                ),
+                // which makes blur
+                BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                  child: Container(
+                    decoration: BoxDecoration(color: Colors.transparent),
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '📍 Aix-en-Provence',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.w300),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        'Good morning new yorkers',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Image.asset('assets/1.png')
+                    ],
+                  ),
                 )
               ],
             ),
